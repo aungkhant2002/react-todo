fe47b2156ef5995d3dfdcc10080d5c161182748d
aungkhant2002 <aung.khant@ucsy.edu.mm>
2025-02-03 20:08:55 +0630

    feat: heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = ()=> {
+    return (
+        <section className="heading">
+            <h1 className="font-bold font-serif text-3xl mb-5">Task Manager</h1>
+        </section>
+    )
+}
+
+export default Heading;
